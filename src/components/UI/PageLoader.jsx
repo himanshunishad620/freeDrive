1da09759a28f3451682cc19005965f8880f9b5aa
Himanshu Nishad <himanshunishad620@gmail.com>
2025-08-08 09:28:33 +0530

    Loading Error Added

--- src/components/UI/PageLoader.jsx
+++ src/components/UI/PageLoader.jsx
@@ -1,22 +1,22 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { FaSpinner } from "react-icons/fa";
 
 export default function PageLoader() {
   const [msg, setMsg] = useState(null);
-  const ref = useRef();
   useEffect(() => {
-    ref.current = setTimeout(() => {
-      setMsg(
-        "It might take few seconds for once because of Render Server which gets inactive in free version!  ",
-      );
-    }, 4000);
-    return clearTimeout(ref.current);
+    setTimeout(
+      () =>
+        setMsg(
+          "It might take few seconds for once because of Render Server which gets inactive in free version!  ",
+        ),
+      4000,
+    );
   }, []);
   return (
     <div className="fixed top-0 left-0 flex h-screen w-screen flex-col items-center justify-center bg-white/10">
       <FaSpinner className="animate-spin text-3xl" />
       <p className="text-lg">Loading....</p>
-      <p>{msg}</p>
+      <p className="text-red-500">{msg}</p>
     </div>
   );
 }
